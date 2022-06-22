87e0f6454916f41072e5633008ffc0a8122910fa
Gabriela Benalcazar <68346985+GabrielaBenalcazar@users.noreply.github.com>
2022-06-22 16:52:28 +0200

    account,CONTACT repository

--- src/main/java/com/ironhack/homework_3/repository/AccountRepository.java
+++ src/main/java/com/ironhack/homework_3/repository/AccountRepository.java
@@ -2,8 +2,30 @@ package com.ironhack.homework_3.repository;
 
 import com.ironhack.homework_3.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Integer> {
+//    The mean employeeCount can be displayed by typing “Mean EmployeeCount”
+        @Query("SELECT AVG(employeeCount) FROM Account")
+        Integer  avgEmployeeCount();
+
+        //    The median employeeCount can be displayed by typing “Median EmployeeCount”
+        @Query("SELECT employeeCount AS ec FROM Account " +  "ORDER BY ec")
+        List<Integer> EmployeeCountList();
+
+        //    The maximum employeeCount can be displayed by typing “Max EmployeeCount”
+        @Query("SELECT MAX(employeeCount) FROM Account")
+        Integer EmployeeCountMax();
+
+        //    The minimum employeeCount can be displayed by typing “Min EmployeeCount”
+        @Query("SELECT MIN(employeeCount) FROM Account")
+        Integer EmployeeCountMin();
+
+
+
+
 }
